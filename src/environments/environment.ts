d360ece9c814ab5db30b4e4335468d963e148285
ALEJANDRO CAMLTASKS <98296383+CAMLTASKS@users.noreply.github.com>
2025-11-05 17:57:13 -0500

    cambio de key

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: false,
-  OPENAI_API_KEY: "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"
+  OPENAI_API_KEY: "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"
 };
